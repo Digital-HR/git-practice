30341727db75393423fed5cad7dbc0733e812b69
jidicula <johanan.idicula@gmail.com>
2020-12-23 14:05:34 -0500

    Add skeleton Python program

---
+++ Main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+def main():
+    print("Hello world!")
+    function1()
+    function2()
+    print("\n")
+
+
+def function1():
+    print("function1 call succeeeded")
+    return
+
+
+def function2():
+    print("function2 call suceeded")
+    return
+
+
+if __name__ == '__main__':
+    main()
