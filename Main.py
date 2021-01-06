44712a9dbdb4c25afbed99da705dc0cd15d055ed
jidicula <johanan.idicula@ecn.forces.gc.ca>
2021-01-06 09:44:52 -0500

    [feat][Main] Add The Great Gatsby
    **Why this change was necessary**
    The Great Gatsby is now in the public domain, so we can use it as a text
    source example!
    
    **What this change does**
    Replaces "Jabberwocky" with an excerpt from "The Great Gatsby".
    
    **Any side-effects?**
    None

--- Main.py
+++ Main.py
@@ -2,53 +2,28 @@
 
 
 def main():
-    jabberwocky = """’Twas brillig, and the slithy toves
-Did gyre and gimble in the wade;
-All mimsy were the borogoves,
-And the mome raths outgrabe.
+    gaatsby = """The silhoouette of a moving cat waavered across the mooonlight, 
+and turning my head to watch it, I saw that I was not alone—fifty feet 
+away a figure had emerged from the shadow of my neighbor's mansion and 
+was standing with his hands in his pockets regarding the silver pepper 
+of the stars. Something in his leisurely movements and the secure 
+position of his feet upon the lawn suggested that it was Mr. Gatsby 
+himself, come out to determine what share was his of our local heavens.
 
-
-
-"Beware the Jabberwock, my son!
-The jaws that bite, the claws that catch!
-Beware the Jubjub bird, and shun
-The frumious Bandersnatch!"
-
-
-He took his vorpal sword in hand:
-Long time the manxome foe he sought—
-So rested he by the Tumtum tree,
-And stood awhile in thought.
-
-
-And as in uffish thought he stood,
-The Jabberwock, with eyes of flame,
-Came whiffling through the tulgey wood,
-And burbled as it came!
-
-
-One, two! One, two! And through and through
-The vorpal blade went snicker-snack!
-He left it dead, and with its head
-He went galumphing back.
-
-
-"And hast thou slain the Jabberwock?
-Come to my arms, my beamish boy!
-O frabjous day! Callooh! Callay!"
-He chortled in his joy.
-
-
-’Twas brillig, and the slithy toves
-Did gyre and gimble in the wabe;
-All mimsy were the borogoves,
-And the mome raths outgrabe."""
+I decided to call to him. Miss Baker had mentioned him at dinner, and that 
+would do for an introduction. But I didn't call to him, for he gave a sudden
+intimation that he was content to be alone—he stretched out his arms toward
+the dark water in a curious way, and, far as I was from him, I could have 
+sworn he was trembling. Involuntarily I glanced seaward—and distinguished 
+nothing except a single green light, minute and far away, that might have been
+the end of a dock. When I looked once more for Gatsby he had vanished, and I
+was alone again in the unquiet darkness."""
     print("Hello world!")
 
     function1()
     function2()
     print("\n")
-    print(jabberwocky)
+    print(gatsby)
 
 
 def function1():
