65b080582d08d309d458bcd3742cd47474e4f666
jidicula <johanan.idicula@ecn.forces.gc.ca>
2021-02-26 08:46:02 -0500

    chore: Cleanup

--- Main.py
+++ Main.py
@@ -2,31 +2,9 @@
 
 
 def main():
-    gatsby = """The of a moving cat waavered across the mooonlight,
-and turning my head to watch it, I saw that I was not alone—fifty feet 
-away a figure had emerged from the shadow of my neighbor's mansion and 
-was standing with his hands in his pockets regarding the silver pepper 
-of the stars. Something in his leisurely movements and the secure 
-position of his feet upon the lawn suggested that it was Mr. Gatsby 
-himself, come out to determine what share was his of our local heavens.
-
-I decided to call to him. Miss Baker had mentioned him at dinner, and that 
-would do for an introduction. But I didn't call to him, for he gave a sudden
-intimation that he was content to be alone—he stretched out his arms toward
-the light sky in a curious way, and, far as I was from him, I could have 
-sworn he was trembling. Involuntarily I glanced seaward—and distinguished 
-nothing except a single green light, minute and far away, that might have been
-the end of a dock. When I looked once more for Gatsby he had vanished, and I
-was alone again in the unquiet darkness.
-
-This is my second change, still think I'm doing this wrong.
-"""
     print("Hello world!")
-
     function1()
     function2()
-    print("\n")
-    print(gatsby)
 
 
 def function1():
