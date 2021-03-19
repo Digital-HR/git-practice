fa7134f1ccb2315963b7710edf654cc326cbde32
jidicula <johanan.idicula@ecn.forces.gc.ca>
2021-03-19 14:30:29 -0400

    fix(leap year): Use correct leap year definition

--- Main.py
+++ Main.py
@@ -11,22 +11,23 @@ def function1():
     print("function1 call succeeded")
     return
 
-def removeDupe(inputList):
-   inputList = list(set(inputList))
-   return inputList
 
+def removeDupe(inputList):
+    inputList = list(set(inputList))
+    return inputList
 
 
 def function2():
     print("function2 call suceeded")
     return
 
+
 def sortedEvens(inputList):
     return sorted([x for x in inputList if (x % 2) == 0])
 
 
 def isLeapYear(year):
-    return year % 4 == 0
+    return (year % 4 == 0) and ((year % 400 == 0) or (year % 100 > 0))
 
 
 if __name__ == "__main__":
